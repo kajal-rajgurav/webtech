7ca13974185fb262df85c3e426228b86b065d14a
kajal-rajgurav <rajguravkajal123@gmail.com>
2026-01-06 20:53:22 +0530

    some

---
+++ CSS/Dummy_Folder/first.js
@@ -0,0 +1,28 @@
+// "use strict"
+
+class bark{
+
+    constructor(){
+        console.log("you now have a new instance of bark class")
+    }
+
+    barks(){
+        console.log("woof");
+    }
+}
+
+const newBark = new bark();
+
+console.log(newBark.barks());
+
+console.log("Hello World");
+
+[1,2].forEach(n => console.log(n))
+
+
+/* 
+this is multiline comment in javascript
+Okay.
+
+*/
+
